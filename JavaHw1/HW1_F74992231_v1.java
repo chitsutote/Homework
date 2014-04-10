020778142a892d96238511a871525279c50e46fa
chitsutote <chitsutote7@gmail.com>
2014-04-10 15:50:17 +0800

    finish first two method:  size , isEmpty

--- JavaHw1/HW1_F74992231_v1.java
+++ JavaHw1/HW1_F74992231_v1.java
@@ -10,27 +10,42 @@ public class HW1_F74992231_v1{
 	//constructor to  initial the variable
 	public HW1_F74992231_v1(){
 		length = 0;
-//		System.out.println("Hello");
+		elements = new int[0];
 	}
 
     //constructor to get input and record
 	public HW1_F74992231_v1(String input){
+		
 		System.out.println(input);
 		System.out.println("printed out all input");
 
 		test = input.split(",");
-		System.out.println(test[0]);
+		
+		elements = new int[test.length];
+		for(int i=0; i< test.length; i++){
+			String text = test[i];
+			int temp = Integer.parseInt(text);
+			elements[i] = temp;
+			System.out.println(temp);
+		}
+		length = elements.length;
+		//System.out.println(test[0]);
 	}
 
 	//Count number
-/*	public int size(){
-	
+	public int size(){
+		return length;
 	}
 
 	public boolean isEmpty(){
-	
+		if( length == 0){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
-
+/*
 	public boolean isPnumber(){
 	
 	}
@@ -71,7 +86,18 @@ public class HW1_F74992231_v1{
 		HW1_F74992231_v1 one = new HW1_F74992231_v1();
 
 		HW1_F74992231_v1 two = new HW1_F74992231_v1(args[0]);
-		System.out.println("Success");
+        
+		int size = two.size();
+		boolean isEmpty = two.isEmpty();
+		System.out.print("第一個數字集合長度:");
+		System.out.println(size);
+		if(isEmpty == true){
+			System.out.print("第一個數字集合是否為空集合:");
+			System.out.println("true");
+		}else{
+			System.out.print("第一個數字集合是否為空集合:");
+			System.out.println("false");
+		}
 	
 	}
 }
