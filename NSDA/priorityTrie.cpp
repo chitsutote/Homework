3605e4d55c0050dab4931c524ceee72792acf4ef
chitsutote <chitsutote7@gmail.com>
2014-06-20 16:17:28 +0800

    read prefix set from file

--- NSDA/priorityTrie.cpp
+++ NSDA/priorityTrie.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#define MAX_PREFIX_NUM 1000
+#include <cstring>
 
 using namespace std;
 
+#define MAX_PREFIX_NUM 1000
+#define MAX_PREFIX_LENGTH 32
+
+
+char prefixSet[MAX_PREFIX_NUM][MAX_PREFIX_LENGTH];
+
+using namespace std;
+
+void BuildPriorityTrie(int);
 
 int main(){
 
-	char prefixSet[MAX_PREFIX_NUM];
+	int count = 0;
 	
 	fstream fin;
 	fin.open("PrefixSet.txt",ios::in);
@@ -17,10 +26,20 @@ int main(){
 		cout << "Can't open file"<<endl;
 	}
 
-	while(fin.getline(prefixSet,sizeof(prefixSet),'\n')){
-		cout<<prefixSet<<endl;
+	while(fin.getline(prefixSet[count],sizeof(prefixSet[count]),'\n')){
+		cout<<prefixSet[count]<<endl;
+		count++;
 	}
 
+	fin.close();
 	return 0;
 
+}
+
+void BuildPriorityTrie(int count){
+/*
+	for(int i = 0; i < count;i++){
+		cout << *prefixSet[i] << " " << strlen(*prefixSet[i]) << endl;
+	}
+*/
 }
